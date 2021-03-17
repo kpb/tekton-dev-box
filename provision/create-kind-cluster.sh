3ed8fd8e33311774dc2318fea53002065c91c7ac
Kenneth Bowen <kenneth@kennethbowen.com>
2021-03-16 20:20:40 -0600

    Create local registry and configure KinD cluster
    - Create registry if it doesn't exist
    - Configure cluster to use it, including Docker network

--- provision/create-kind-cluster.sh
+++ provision/create-kind-cluster.sh
@@ -1,13 +1,27 @@
 #!//usr/bin/env bash
 #
-# Creates a KinD cluster with NGINX ingress. Uncomment the -role lines to create a multi-node cluster.
+# Creates a local docker registry, configures a KinD cluster with NGINX ingress. Uncomment the -role lines to create a multi-node cluster.
 
 set -e
-set -x
+
+# create registry container unless it already exists
+registry_name='kind-registry'
+registry_port='5000'
+running="$(docker inspect -f '{{ .State.Running }}' "${registry_name}" 2>/dev/null || true)"
+
+if [ "${running}" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${registry_port}:5000" --name "${registry_name}" \
+    registry:2
+fi
 
 cat <<EOF | kind create cluster --wait 10m --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${registry_port}"]
+    endpoint = ["http://${registry_name}:${registry_port}"]
 nodes:
 - role: control-plane
   kubeadmConfigPatches:
@@ -31,3 +45,20 @@ EOF
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller  --timeout=90s
+
+# connect the registry
+docker network connect "kind" "${registry_name}" || true
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${registry_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
